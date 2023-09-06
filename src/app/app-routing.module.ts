835a23e4ebe34e33e63c69a85104901bc1cf5cf1
Victor Bonin <victorduvercors@gmail.com>
2023-09-06 20:56:29 +0200

    components initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VesselsComponent } from './vessels/vessels.component';
+import { EmissionsComponent } from './emissions/emissions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "vessels", component: VesselsComponent},
+  {path: "emissions", component: EmissionsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-component/app.component';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VesselsComponent } from './vessels/vessels.component';
+import { EmissionsComponent } from './emissions/emissions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VesselsComponent,
+    EmissionsComponent
   ],
   imports: [
     BrowserModule,
